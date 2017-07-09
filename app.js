459672d0b6f4b8626bfe1e0fa6aed8185424714c
AaronChelvan <aaron.chelvan@outlook.com>
2017-07-09 04:14:31 -0700

    Add login and signup page templates

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var express = require("express");
 var app = express();
 var nunjucks = require("nunjucks");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+	secret: "blah blah top secret :D",
+	resave: false,
+	saveUninitialized: false
+}));
 
 //Configure Nunjucks
 nunjucks.configure("views", {
@@ -26,24 +37,65 @@ var messageSchema = new mongoose.Schema({
 });
 var Message = mongoose.model("Message", messageSchema);
 
-//Home/Login page
-app.get("/", function (req, res) {
+//Set up Passport
+/*app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());*/
+
+//Default Route
+//If not logged in, go to 'home.html'
+//If logged in, go to 'messages.html'
+app.get("/", function(req, res){
 	//List all users
-	User.find({}, function(error, users){
+	/*User.find({}, function(error, users){
 	    if (error) {
 	        console.log(error);
 	    } else {
 	        console.log(users);
 	    }
-	});
+	});*/
 	var name = "Aaron";
-	res.render("index.html", {name});
+	res.render("home.html", {name});
 });
 
-app.get("\*", function(req, res) {
+//Login Routes
+app.get("/login", function(req, res){
+	res.render("login.html");
+});
+app.post("/login", function(req,res){
+
+});
+
+//Sign Up Routes
+app.get("/signup", function(req, res){
+	res.render("signup.html");
+});
+app.post("/signup", function(req, res){
+
+});
+
+
+
+
+app.get("/home", function(req, res){
+	//List all users
+	/*User.find({}, function(error, users){
+	    if (error) {
+	        console.log(error);
+	    } else {
+	        console.log(users);
+	    }
+	});*/
+	var name = "Aaron";
+	res.render("home.html", {name});
+});
+
+app.get("\*", function(req, res){
     res.render("error.html");
+	console.log("ERROR");
 });
 
-app.listen(3000, function () {
+app.listen(3000, function(){
 	console.log("Listening on port 3000!")
 });

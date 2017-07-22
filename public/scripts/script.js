a2bbe0ff33329659aca7804e184fcaaf245bbea8
AaronChelvan <aaron.chelvan@outlook.com>
2017-07-21 19:18:58 -0700

    Log in and sign up merged into single page

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,18 @@
+// Login/signup screen
+$("#loginSelect").click(function(){
+	$(this).addClass("active");
+	$("#signupSelect").removeClass("active");
+	$(".loginForm").show();
+	$(".signupForm").hide();
+});
+
+$("#signupSelect").click(function(){
+	$(this).addClass("active");
+	$("#loginSelect").removeClass("active");
+	$(".loginForm").hide();
+	$(".signupForm").show();
+});
+
 //Hide/show the text of a message by clicking on the info bar of that message
 $(".messageInfo").click(function(){
 	$(this).next().toggle();
